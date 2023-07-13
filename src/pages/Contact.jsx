9325873bd4e4d3cdbebe1d27487e9742742b165e
0rigin-c0de <shunnu807@gmail.com>
2023-07-13 23:23:22 +0530

    add contactpage

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Contactpage from "../components/Contactpage/Contactpage";
+import Header from "../components/Header/Navbar";
+
+function Contact() {
+  return (
+    <div>
+      <Header />
+      <Contactpage />
+    </div>
+  );
+}
+
+export default Contact;
