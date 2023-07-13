b0506422261a334e93f94aa38e85c46f3271c272
0rigin-c0de <shunnu807@gmail.com>
2023-07-13 23:23:09 +0530

    add homepage

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "../components/Header/Navbar";
+import Homepage from "../components/Homepage/Hompage";
+// import Aboutpage from "../components/Aboutpage/Aboutpage";
+// import Skills from "../components/Skills/Myskills";
+// import Projectlist from "../components/Projectlist/Projectlist";
+// import Contactpage from "../components/Contactpage/Contactpage";
+function Home() {
+  return (
+    <div>
+      <Header />
+      <Homepage />
+      {/* <Aboutpage />
+      <Skills />
+      <Projectlist />
+      <Contactpage /> */}
+    </div>
+  );
+}
+
+export default Home;
