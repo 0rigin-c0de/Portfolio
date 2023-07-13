8fa645da08b0baba371a60ea0dd1378a972dc63c
0rigin-c0de <shunnu807@gmail.com>
2023-07-13 23:23:45 +0530

    add projectspage

---
+++ src/pages/Project.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header/Navbar";
+import Projectlist from "../components/Projectlist/Projectlist";
+
+function Project() {
+  return (
+    <div>
+      <Header />
+      <Projectlist />
+    </div>
+  );
+}
+
+export default Project;
