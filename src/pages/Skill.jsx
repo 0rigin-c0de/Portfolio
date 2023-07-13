c346bc068b12a2893a4c21092c79e71fb4d22073
0rigin-c0de <shunnu807@gmail.com>
2023-07-13 23:23:55 +0530

    add skillspage

---
+++ src/pages/Skill.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header/Navbar";
+import Skills from "../components/Skills/Myskills";
+
+const MySkills = () => {
+  return (
+    <>
+      <Header />
+      <Skills />
+    </>
+  );
+};
+
+export default MySkills;
