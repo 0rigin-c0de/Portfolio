807ff99cfc11404dc5a1cba8236add759302299f
0rigin-c0de <shunnu807@gmail.com>
2023-07-13 23:23:36 +0530

    add aboutpage

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Aboutpage from "../components/Aboutpage/Aboutpage";
+import Header from "../components/Header/Navbar";
+
+function About() {
+  return (
+    <div>
+      <Header />
+      <Aboutpage />
+    </div>
+  );
+}
+
+export default About;
